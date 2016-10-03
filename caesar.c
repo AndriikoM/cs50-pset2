6c2d9a46a2d7b47ff244fc480a3d4523c0801541
AndriikoM <morozandrii95@gmail.com>
2016-10-03 16:12:07 +0300

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -10,29 +10,25 @@ int main(int argc, string argv[])
      if (argc !=2)
     {
         printf("You didn`t enter key. \n ");
-        return 0;
+        return 1;
     }
     key = atoi(argv[1]);
     text = GetString();
-    for (int i = 0, l = strlen(text); i < l; i++)
+    for (int i = 1, l = strlen(text); i < l; i++)
     {
         if (isalpha(text[i]))
         {
             if (isupper(text[i]))
             {
-                // 
-                text[i] = (text[i] - 'A' + key) % 26;
-                // 
-               text[i] = text[i] + 'A';
+                text[i] = ((text[i] - 'A' + key) % 26) + 'A';
             }
             else
             {
-                // 
-                text[i] = (text[i] - 'a' + key) % 26;
-                // 
-               text[i] = text[i] + 'a';
+                text[i] = (text[i] - 'a' + key) % 26 + 'a';
             }
         }
         printf("%c",text[i]);
     }
+    printf("/n");
+    return 0;
 }
